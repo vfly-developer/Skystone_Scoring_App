218836e3b2d2874ddffa4390c0bd5ad85b19a511
Vinh Nguyen <vinh16488@gmail.com>
2019-12-30 19:39:32 -0500

    Initial Commit

---
+++ PracticeV1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PracticeV1
+//
+//  Created by Vinh Nguyen on 12/30/19.
+//  Copyright © 2019 Vinh Nguyen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
